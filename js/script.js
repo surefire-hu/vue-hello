baffeb4376244ed9aa8a40ddbf2f067b3b128101
surefire-hu <surefire.hu2001@gmail.com>
2024-08-29 09:55:40 +0200

    aggiunta immagine

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 
-  const { createApp, ref } = Vue
+  const { createApp } = Vue
 
   createApp({
-    setup() {
-      const message = ref('Hello World!')
-      return {
-        message
-      }
+        data() {
+            return{
+            message: 'Hello World!',
+            imageSrc: 'https://images.unsplash.com/photo-1703248187251-c897f32fe4ec?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cGFuZGElMjBiZWFyfGVufDB8MXwwfHx8Mg%3D%3D'
+        }
     }
   }).mount('#app')
+
+  
